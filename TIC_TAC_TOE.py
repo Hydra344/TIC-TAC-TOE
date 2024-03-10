b15a05f933a80ce6ec0658d36e56749c983b633a
Hydra344 <148814489+Hydra344@users.noreply.github.com>
2024-03-10 18:49:32 +0100

    TIC_TAC_TOE.py

--- TIC_TAC_TOE.py
+++ TIC_TAC_TOE.py
@@ -69,9 +69,6 @@ def Button(Z):
                 WinnerD = Winner
                 LW['text'] = "Winner is: " + WinnerD
 
-
-
-
     if Winner != "":
         Z = 1
         Winner = ""
@@ -80,42 +77,24 @@ def Button(Z):
             Arr[i]['state'] = "normal"
             Z = Z + 1
 
-QuitB = tk.Button(height=1,width=18,borderwidth=10, text="Quit",bg=('#2b2b2b'),fg=('#ff0059'),font=('Arial'),command=lambda: root.destroy())
+QuitB = tk.Button(height=1,width=18,borderwidth=10, text="Quit",bg=('#2b2b2b'),fg=('#ff0059'),font=('Arial'),command=lambda: root.destroy()).grid(row=0, column =1)
 
-LA = tk.Label(text=("Tic Tac Toe"),height=1,width=10,bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial'))
-LW = tk.Label(text=(""),height=1,width=15,bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial'))
-LC = tk.Label(text=("Current Player: "+ P),height=1,width=20,bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial'))
+LA = tk.Label(text=("Tic Tac Toe"),height=1,width=10,bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial')).grid(row=0, column =2)
+LW = tk.Label(text=(""),height=1,width=15,bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial')).grid(row=1, column =2)
+LC = tk.Label(text=("Current Player: "+ P),height=1,width=20,bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial')).grid(row=1, column =1)
 
-B0 = tk.Button(height=1,width=3,text="1",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(0))
-B1 = tk.Button(height=1,width=3,text="2",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(1))
-B2 = tk.Button(height=1,width=3,text="3",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(2))
+B0 = tk.Button(height=1,width=3,text="1",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(0)).grid(row=2, column =1)
+B1 = tk.Button(height=1,width=3,text="2",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(1)).grid(row=2, column =2)
+B2 = tk.Button(height=1,width=3,text="3",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(2)).grid(row=2, column =3)
 
-B3 = tk.Button(height=1,width=3,text="4",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(3))
-B4 = tk.Button(height=1,width=3,text="5",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(4))
-B5 = tk.Button(height=1,width=3,text="6",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(5))
+B3 = tk.Button(height=1,width=3,text="4",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(3)).grid(row=3, column =1)
+B4 = tk.Button(height=1,width=3,text="5",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(4)).grid(row=3, column =2)
+B5 = tk.Button(height=1,width=3,text="6",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(5)).grid(row=3, column =3)
 
-B6 = tk.Button(height=1,width=3,text="7",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(6))
-B7 = tk.Button(height=1,width=3,text="8",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(7))
-B8 = tk.Button(height=1,width=3,text="9",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(8))
+B6 = tk.Button(height=1,width=3,text="7",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(6)).grid(row=4, column =1)
+B7 = tk.Button(height=1,width=3,text="8",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(7)).grid(row=4, column =2)
+B8 = tk.Button(height=1,width=3,text="9",bg=('#2b2b2b'),fg=('#03a1fc'),font=('Arial',100), command=lambda: Button(8)).grid(row=4, column =3)
 
 Arr = [B0, B1, B2, B3, B4, B5, B6, B7, B8]
-
-QuitB.grid(row=0, column =1)
-LA.grid(row=0, column =2)
-
-LC.grid(row=1, column =1)
-LW.grid(row=1, column =2)
-
-B0.grid(row=2, column =1)
-B1.grid(row=2, column =2)
-B2.grid(row=2, column =3)
-
-B3.grid(row=3, column =1)
-B4.grid(row=3, column =2)
-B5.grid(row=3, column =3)
-
-B6.grid(row=4, column =1)
-B7.grid(row=4, column =2)
-B8.grid(row=4, column =3)
 
 root.mainloop()
